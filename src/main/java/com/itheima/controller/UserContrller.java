1984afce663d79396f241747f1f21a6adb7485cb
mrliyan143 <1119335376@qq.com>
2022-06-29 00:24:01 +0800

    整合ssm

---
+++ src/main/java/com/itheima/controller/UserContrller.java
@@ -0,0 +1,12 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserContrller {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
